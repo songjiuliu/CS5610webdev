31858d0d76f22e81acfe18ba29bead31b9bcd104
songjiuliu <songjiuliu@hotmail.com>
2020-01-25 23:58:39 -0500

    add edit and update function

--- src/main/webapp/admin-user/user-admin.controller.client.js
+++ src/main/webapp/admin-user/user-admin.controller.client.js
@@ -2,8 +2,10 @@ jQuery(function () {
 
 let createuser = $("#createuser"); // put $ remember jquery object
 let deleteuser = $(".wbdv-remove");
+let edituser = $(".wbdv-edit");
+let updateuser = $(".wbdv-update");
 let users = [];
-let userList = $("#user-list")
+
 var userService = new AdminUserServiceClient();
 let rowTemplate = jQuery('.wbdv-template');
 
@@ -23,7 +25,7 @@ function deleteUser (event){
 	td = currenttarget.parent().parent().parent().parent()
 	
 	userId=td.find('.wbdv-id').html()
-	console.log(userId)
+	//console.log(userId)
 	userService.deleteUser(userId)
             .then(response => {
 
@@ -33,16 +35,54 @@ function deleteUser (event){
             
 }
 let currentUserId = -1
-const editUser = index => {
-    const userId = users[index]._id;
-    currentUserId = userId;
-    userService.findUserById(userId)
-        .then(user => {
-            console.log(user)
-            usernameFld.val(user.username)
-        })
-}
+function editUser (event){
 
+
+	currenttarget=$(event.currentTarget)
+	td = currenttarget.parent().parent().parent().parent()
+	userId=td.find('.wbdv-id').html()
+	
+	console.log(userId)
+	userService.findUserById(userId)
+        .then(user => {
+
+            users=[]
+            users.push(user)
+            renderUsers()
+            currentUserId=userId
+
+        })
+
+            
+}
+const updateUser = () => {
+	const username = usernamefld.val()
+    usernamefld.val("")
+    const firstname = firstnamefld.val()
+    firstnamefld.val("")
+    const lastname = lastnamefld.val()
+    lastnamefld.val("")
+    const role = rolefld.val()
+    const password = passwordfld.val()
+    passwordfld.val("")
+    var user = {
+            username:username,
+            password:password,
+            firstname:firstname,
+            lastname:lastname,
+            password:password,
+            role:role
+        }
+	console.log(user)
+	console.log(currentUserId)
+        userService.updateUser(currentUserId, user)
+            .then(newUser => {
+                findAllUsers()
+            })
+
+    }
+updateuser = $(".wbdv-update");
+updateuser.click(updateUser)
 const renderUsers = () =>{
 	tbody.empty()
 
@@ -50,7 +90,7 @@ const renderUsers = () =>{
 		const user = users[u]
 		console.log(user)
 		const rowClone =rowTemplate.clone()
-		console.log(rowClone)
+		//console.log(rowClone)
 		rowClone.removeClass('wbdv-hidden')
 		rowClone.find('.wbdv-username').html(users[u].username)
 		rowClone.find('.wbdv-first-name').html(users[u].firstname)
@@ -63,22 +103,17 @@ const renderUsers = () =>{
 	deleteuser = $(".wbdv-remove");
 	//console.log(deleteuser)
 	deleteuser.click(deleteUser)
+	edituser = $(".wbdv-edit");
+	edituser.click(editUser)
+	
 	}
 renderUsers()
-const updateUser = () => {
-        const username = usernameFld.val()
-        usernameFld.val("")
 
-        userService.updateUser(currentUserId, {username: username})
-            .then(newUser => {
-
-                findAllUsers()
-            })
-
-    }
 deleteuser = $(".wbdv-remove");
-
 deleteuser.click(deleteUser)
+
+
+
 createuser = $("#createuser");
 
 function createUser(){
@@ -100,7 +135,7 @@ function createUser(){
             password: password,
             firstname: firstname,
             lastname: lastname,
-            passworc:password,
+            password:password,
             role:role
         }
 	userService
@@ -117,7 +152,6 @@ const findAllUsers = () =>
 userService.findAllUsers()
     .then((theUsers) => {
         users = theUsers
-
         renderUsers()
         })
 
